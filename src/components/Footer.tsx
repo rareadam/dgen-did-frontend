c242fbda1f6d9ddbc0dc7439aed0bccbdba5b94b
Adam Rareships <adam.rareships@gmail.com>
2024-03-19 12:12:44 +0100

    make footer always be at the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Text, Flex } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box as="footer" width="full" py="5" >
+    <Box as="footer" width="full" py="5" position="fixed" bottom="0" left="0" right="0">
       <Flex justifyContent="center" alignItems="center">
         <Text fontSize="md">
             no copyright, fully decentralized and made with ❤️ for self sovereign identity
